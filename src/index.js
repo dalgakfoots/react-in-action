ac0f0554847998384ff4598949e5e9fc4bbbe8cc
dalgakfoots <77476939+dalgakfoots@users.noreply.github.com>
2022-12-26 09:34:03 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import { render } from "react-dom";
+
+const node = document.querySelector("#root");
+const root = React.createElement(
+  "div",
+  {},
+  React.createElement(
+    "h1",
+    {},
+    "Hello, world!",
+    React.createElement(
+      "a",
+      { href: "mailto:mark@ifelse.io" },
+      React.createElement("h1", "React In Action"),
+      React.createElement("em", {}, "... and now it really is!")
+    )
+  )
+);
+
+render(root, node);
